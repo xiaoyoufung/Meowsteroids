65715880b0bb555c56bd7193e810683474354eda
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-10-19 10:07:32 +0700

    set primaryStage

--- src/main/java/se233/meowsteroids/Launcher.java
+++ src/main/java/se233/meowsteroids/Launcher.java
@@ -1,12 +1,16 @@
 package se233.meowsteroids;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Scene scene = new Scene(new Pane());
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
