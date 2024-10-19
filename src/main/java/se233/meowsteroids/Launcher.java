e44af299093939edf3afe55d5a69f29927868e42
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-10-19 09:40:13 +0700

    add Pane class to view folder

--- src/main/java/se233/meowsteroids/Launcher.java
+++ src/main/java/se233/meowsteroids/Launcher.java
@@ -1,4 +1,12 @@
 package se233.meowsteroids;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
