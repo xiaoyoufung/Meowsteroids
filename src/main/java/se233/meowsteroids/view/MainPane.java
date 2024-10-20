920cdd6f88d60fbdb06472a9791e5007098276f3
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-10-20 23:11:07 +0700

    set up MainPane

--- src/main/java/se233/meowsteroids/view/MainPane.java
+++ src/main/java/se233/meowsteroids/view/MainPane.java
@@ -1,7 +1,21 @@
 package se233.meowsteroids.view;
 
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class MainPane extends VBox {
+    public MainPane(){}
 
+    private Pane getDetailsPane(){
+        Pane mainInfoPane = new AnchorPane();
+
+        return mainInfoPane;
+    }
+
+    public void drawPane(){
+        Pane mainInfoPane = getDetailsPane();
+        this.setStyle("-fx-background-color:#FFF;");
+        this.getChildren().add(mainInfoPane);
+    }
 }
