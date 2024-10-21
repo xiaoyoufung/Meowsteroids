6f3b5f8f4e07f6cda084b5e970a06b2a42023af6
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-10-21 17:44:52 +0700

    finish genTopArea

--- src/main/java/se233/meowsteroids/view/MainPane.java
+++ src/main/java/se233/meowsteroids/view/MainPane.java
@@ -1,19 +1,20 @@
 package se233.meowsteroids.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 public class MainPane extends VBox {
     public MainPane(){}
 
     private Pane getDetailsPane(){
-        Pane mainInfoPane = new AnchorPane();
-        HBox topArea = genTopArea();
+        BorderPane mainInfoPane = new BorderPane();
+        Pane topArea = genTopArea();
 
-        mainInfoPane.getChildren().addAll(topArea);
+        //mainInfoPane.getChildren().addAll(topArea);
+        mainInfoPane.setTop(topArea);
+        mainInfoPane.setPadding(new Insets(0, 60, 0, 60));
         return mainInfoPane;
     }
 
@@ -23,16 +24,28 @@ public class MainPane extends VBox {
         this.getChildren().add(mainInfoPane);
     }
 
-    private HBox genTopArea(){
-        HBox topArea = new HBox();
+    private Pane genTopArea(){
+        BorderPane topArea = new BorderPane();
+        HBox scoreArea = new HBox(10);
+
         Label highScoreLbl, scoreLbl;
 
         highScoreLbl = new Label("Highscore: ");
-        highScoreLbl.setStyle("-fx-font-size: 30");
+        highScoreLbl.setStyle("-fx-font-size: 48; -fx-font-weight: 300");
 
-        scoreLbl = new Label("0");
+        scoreLbl = new Label("10982");
+        scoreLbl.setStyle("-fx-font-size: 55");
 
-        topArea.getChildren().addAll(highScoreLbl, scoreLbl);
+        scoreArea.getChildren().addAll(highScoreLbl, scoreLbl);
+        scoreArea.setAlignment(Pos.CENTER);
+
+        topArea.setRight(scoreArea);
         return topArea;
+    }
+
+    private VBox genMiddleArea(){
+        VBox middleArea = new VBox(10);
+
+        return middleArea;
     }
 }
