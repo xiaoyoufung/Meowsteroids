4543793863151db0adbcb56fff6216d08338db4e
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-10-21 18:14:22 +0700

    import + adjust BossCat size

--- src/main/java/se233/meowsteroids/view/MainPane.java
+++ src/main/java/se233/meowsteroids/view/MainPane.java
@@ -3,7 +3,10 @@ package se233.meowsteroids.view;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import se233.meowsteroids.Launcher;
 
 public class MainPane extends VBox {
     public MainPane(){}
@@ -11,9 +14,11 @@ public class MainPane extends VBox {
     private Pane getDetailsPane(){
         BorderPane mainInfoPane = new BorderPane();
         Pane topArea = genTopArea();
+        Pane middleArea = genMiddleArea();
 
         //mainInfoPane.getChildren().addAll(topArea);
         mainInfoPane.setTop(topArea);
+        mainInfoPane.setCenter(middleArea);
         mainInfoPane.setPadding(new Insets(0, 60, 0, 60));
         return mainInfoPane;
     }
@@ -46,6 +51,11 @@ public class MainPane extends VBox {
     private VBox genMiddleArea(){
         VBox middleArea = new VBox(10);
 
+        ImageView catBossImg = new ImageView(new Image((Launcher.class.getResource("assets/images/BossCat.png")).toString()));
+        catBossImg.setFitWidth(260);
+        catBossImg.setFitHeight(260);
+
+        middleArea.getChildren().addAll(catBossImg);
         return middleArea;
     }
 }
