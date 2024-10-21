dbc8ed4124a261291b20c4450cbd5f91d13f7026
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-10-21 16:08:13 +0700

    connect MainPane with Launcher

--- src/main/java/se233/meowsteroids/Launcher.java
+++ src/main/java/se233/meowsteroids/Launcher.java
@@ -2,19 +2,30 @@ package se233.meowsteroids;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import se233.meowsteroids.view.MainPane;
 
 public class Launcher extends Application {
+    private static MainPane mainPane = null;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Scene scene = new Scene(new Pane(), 1000, 600);
+        mainPane = new MainPane();
+
+        refreshPane();
+
+        Scene scene = new Scene(mainPane, 1000, 600);
 
         // Apply CSS
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static void refreshPane(){
+        mainPane.drawPane();
     }
 }
--- src/main/java/se233/meowsteroids/view/MainPane.java
+++ src/main/java/se233/meowsteroids/view/MainPane.java
@@ -1,6 +1,8 @@
 package se233.meowsteroids.view;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -9,7 +11,9 @@ public class MainPane extends VBox {
 
     private Pane getDetailsPane(){
         Pane mainInfoPane = new AnchorPane();
+        HBox topArea = genTopArea();
 
+        mainInfoPane.getChildren().addAll(topArea);
         return mainInfoPane;
     }
 
@@ -17,5 +21,18 @@ public class MainPane extends VBox {
         Pane mainInfoPane = getDetailsPane();
         this.setStyle("-fx-background-color:#FFF;");
         this.getChildren().add(mainInfoPane);
+    }
+
+    private HBox genTopArea(){
+        HBox topArea = new HBox();
+        Label highScoreLbl, scoreLbl;
+
+        highScoreLbl = new Label("Highscore: ");
+        highScoreLbl.setStyle("-fx-font-size: 30");
+
+        scoreLbl = new Label("0");
+
+        topArea.getChildren().addAll(highScoreLbl, scoreLbl);
+        return topArea;
     }
 }
