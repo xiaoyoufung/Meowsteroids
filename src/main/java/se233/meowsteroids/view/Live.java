e183f6b9cef9300b62e0e72119cd8bd842556f29
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-10-25 12:24:55 +0700

    add Live class to modify game life

--- src/main/java/se233/meowsteroids/view/Live.java
+++ src/main/java/se233/meowsteroids/view/Live.java
@@ -1,7 +1,25 @@
 package se233.meowsteroids.view;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 public class Live extends Pane {
+    Label live, liveLbl;
+    HBox liveArea =  new HBox(10);
 
+    public Live(){
+        liveLbl = new Label("Lives: ");
+        liveLbl.setStyle("-fx-font-size: 48; -fx-font-weight: 300");
+
+        live = new Label("0");
+        live.setStyle("-fx-font-size: 55; -fx-color: red");
+
+        liveArea.getChildren().addAll(liveLbl, live);
+        getChildren().add(liveArea);
+    }
+
+    public void setLive(int live){
+        this.live.setText(Integer.toString(live));
+    }
 }
